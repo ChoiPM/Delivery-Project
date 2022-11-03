42c33eaab055b4667d896706189916badce9819d
dkdud <dkdud604@gmail.com>
2022-11-03 15:03:12 +0900

    add dkdud

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("변아영구");
-        System.out.println("최호림 개멍청이");
+        System.out.println("변아영");
 
     }
 }
