b4c5e99a3f7c12615c5597c31490f28f49b5e14b
dkdud <dkdud604@gmail.com>
2022-11-03 14:10:30 +0900

    dkdudrn

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("변아영수정");
-        System.out.println("변아영개멍청");
+        System.out.println("변아영구");
 
     }
 }
