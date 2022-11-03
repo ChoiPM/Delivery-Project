433130ef0f5a0107bd8411ead0d5887e056d8d1b
dkdud <dkdud604@gmail.com>
2022-11-03 14:03:46 +0900

    dkdud: d

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,10 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("12");
-        System.out.println("34");
-        System.out.println("56");
-        System.out.println("67");
-        System.out.println("678");
+        System.out.println("변아영수정");
 
     }
 }
