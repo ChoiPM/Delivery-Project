8551a840984e41cca67a54b1d976934cb3de52d1
Horim Choi <horimnarna@naver.com>
2022-11-07 21:42:19 +0900

    Horim

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("12");
-        System.out.println("34");
+        System.out.println("Horim");
     }
 }
