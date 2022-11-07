3bc13dbc6016bcae276778ae65cc90cb9fbc9061
JangSeonHye <1401hoo@naver.com>
2022-11-07 22:02:21 +0900

    jang

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,11 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Jang");
         System.out.println("Lee");
-<<<<<<< HEAD
         System.out.println("JJang");
-=======
         System.out.println("이소림");
-
->>>>>>> 326d770db259cf60747005eba87840f08c3dee20
     }
 }
