2f985ddba4a3f00cb47ab244aeee30d377d7c2e2
dkdud <dkdud604@gmail.com>
2022-11-07 22:54:29 +0900

    Ayeong

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,10 +4,8 @@ public class Test {
         System.out.println("34");
         System.out.println("Jang");
         System.out.println("Lee");
-<<<<<<< HEAD
-=======
+
         System.out.println("JJang");
         System.out.println("이소림");
->>>>>>> 3bc13dbc6016bcae276778ae65cc90cb9fbc9061
     }
 }
